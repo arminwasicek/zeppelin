0ea7cdfb4a8357e219ecd6741a777152ecdcc6eb
Armin Wasicek <arminw@sumologic.com>
2018-01-11 09:15:29 -0800

    added unit testing for java

---
+++ sumo/src/test/java/org/apache/zeppelin/sumo/util/ParseDateTest.java
@@ -0,0 +1,11 @@
+package org.apache.zeppelin.sumo.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ParseDateTest {
+    @Test
+    public void testConcatenate() {
+        assertEquals("onetwo", "onetwo");
+    }
+}
