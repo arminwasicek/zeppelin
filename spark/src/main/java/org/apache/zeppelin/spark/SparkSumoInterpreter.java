cf6c711de4eb344b9eb2d8a9fcbb13168c7c5abc
Armin Wasicek <arminw@sumologic.com>
2018-01-13 18:30:02 -0800

    added class

---
+++ spark/src/main/java/org/apache/zeppelin/spark/SparkSumoInterpreter.java
@@ -0,0 +1,12 @@
+package org.apache.zeppelin.spark;
+
+import java.util.Properties;
+
+/**
+ * 
+ */
+public class SparkSumoInterpreter extends SparkSqlInterpreter {
+  public SparkSumoInterpreter(Properties property) {
+    super(property);
+  }
+}
